54dce0ae75bb3611cf0eac63173fc8dc71ded17c
diego2000 <diego2000@gmail.com>
2020-11-08 15:55:16 -0600

    archivos que no se usan

--- src/main/Menu.js
+++ src/main/Menu.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import "./styles/menu.css"
-import {Link, withRouter, useHistory} from "react-router-dom"
+import {Link, useHistory} from "react-router-dom"
 
  const isActive = (history, path) =>{
    console.log(history)
